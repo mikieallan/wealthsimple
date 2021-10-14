19029a50b3275029ebf68ddc64180e6a7b72ad20
Mikie Allan <mikieallan@gmail.com>
2021-10-14 16:11:06 -0400

    Updated cli UX

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if(DEBUG_MODE):
 	tradeToken = "Bearer gAFS16IQEBCs-2Jim1sfLDI7jFYkHZjSubuttJ4W05k"
 
 else:
-	USE_ENV = str(input("\n\nDo you want to use preconfigured values (y/n)?\n>>> "))
+	USE_ENV = str(input("\n\nDo you want to use preconfigured values (Y/N)?\n>>> ")).upper()
 
-	if USE_ENV == 'y':
+	if USE_ENV == 'Y':
 		try:
 			token = wSimple.login()
 		except:
@@ -51,72 +51,91 @@ my_holdings = wSimple.getAccountHoldings(tradeToken)
 
 my_holdings = wSimple.getPositions(tradeToken, my_holdings)
 
-if False:
+to_print = ""
 
-	to_print = str(input("\n\nWhat do you want to see, totals or holdings?\n>>> ")).strip().upper()
+while True:
+	print("\n\nWhat would like to do?\n  A. View Account Totals\n  \
+B. View Holdings\n  C. Purchase to balance portfolio\n  D. End")
+	to_print = str(input("\n>>> ")).strip().upper()
 
-	if to_print == "TOTALS":
+	if to_print == "A":
 		for key in my_holdings:
 			print(my_holdings[key]['type'] + ": " + str(my_holdings[key]['total']))
+		print("\n=====")
+		continue
 
-	elif to_print == "HOLDINGS":
-		pprint.pprint(my_holdings)
-
-my_holding_keys = list(my_holdings.keys())
-
-while (True):
-	try:
-		ch = 'A'
-		print("\n\nWhich account would you like to transact in?\n")
+	elif to_print == "B":
 		for key in my_holdings:
-			print("  " + ch + ". " + my_holdings[key]['type'] + ", $" + str(my_holdings[key]['cash']['value']) + " available in cash")
-			ch = chr(ord(ch) + 1)
+			print(my_holdings[key]['type'] + ": " + str(my_holdings[key]['total']))
+			for key2 in my_holdings[key]:
+				if (type(my_holdings[key][key2]) == dict):
+					if (key2 == "cash"):
+						print("  Cash: " + str(my_holdings[key][key2]['value']))
+					else:
+						print("  " + my_holdings[key][key2]['symbol'] + ": " + str(my_holdings[key][key2]['value']) + ", Price: " + str(round(float(my_holdings[key][key2]['price']),2)))
+		print("\n=====")
+		continue
 
-		index = ord(input("\n>>> ").upper()) - 65
+	elif to_print == "C":
+		my_holding_keys = list(my_holdings.keys())
 
-		account_of_interest = my_holding_keys[index]
+		while (True):
+			try:
+				ch = 'A'
+				print("\n\nWhich account would you like to transact in?\n")
+				for key in my_holdings:
+					print("  " + ch + ". " + my_holdings[key]['type'] + ", $" + str(my_holdings[key]['cash']['value']) + " available in cash")
+					ch = chr(ord(ch) + 1)
+
+				index = ord(input("\n>>> ").upper()) - 65
+				account_of_interest = my_holding_keys[index]
+				break
+
+			except: 
+				print("Invalid entry. Please choose a valid option from the list.")
+				continue
+
+
+		etf_id_dict= wSimple.getCurrentPortfolioBalance(account_of_interest, etf_dict, my_holdings)
+
+
+		amount_to_contribute = int(input("\n\nAmount to use:\n>>> "))
+
+		while amount_to_contribute >= my_holdings[account_of_interest]['cash']['value']:
+			print("You don't have enough cash to invest that much.\n")
+			amount_to_contribute = int(input("Enter a valid amount to use:\n>>> "))
+
+		# Hard coded value whether to print deltas
+		etfs_to_purchase, money_remaining = wSimple.getSharesToPurchase(my_holdings[my_holding_keys[0]], etf_id_dict, amount_to_contribute, False)
+
+
+		print("\n\nIf you want to use $" + str(amount_to_contribute) + ", you should purchase:\n")
+		for key in etfs_to_purchase:
+			if (etf_id_dict[key]['to_buy'] != 0):
+				print(etf_id_dict[key]['symbol'] + ": " + str(etf_id_dict[key]['to_buy']) + " at $" + str(etf_id_dict[key]['price']) + " for " + str(round(etf_id_dict[key]['to_buy'] * etf_id_dict[key]['price'],2)))
+
+		print("\nWhich will leave $" + str(round(money_remaining,2)) + " uninvested out of the $" + str(amount_to_contribute) + " total.")
+
+		to_purchase = input(str("\n\nDo you want to buy the above securities (y / n) in your "+ my_holdings[account_of_interest]['type'] + "?\n>>> ")).upper()
+
+		if (to_purchase == "Y"):
+			print("\n")
+			for key in etfs_to_purchase:
+				if (etf_id_dict[key]['to_buy'] != 0):
+					acc_id = account_of_interest
+					sec_id = key
+					quantity = etf_id_dict[key]['to_buy']
+					price = etf_id_dict[key]['price']
+					data = wSimple.buy(tradeToken, acc_id, sec_id, quantity, price)
+					print(data['symbol'] + ": " + str(data['quantity']) + " purchased from " + data['account_id'])
+
+		else:
+			print("\nNothing purchased\n")
 
 		break
 
-	except: 
-		print("Invalid entry. Please choose a valid option from the list.")
+	elif to_print == "D":
+		break
 
-		continue
-
-
-etf_id_dict= wSimple.getCurrentPortfolioBalance(account_of_interest, etf_dict, my_holdings)
-
-
-amount_to_contribute = int(input("\n\nAmount to use:\n>>> "))
-
-while amount_to_contribute >= my_holdings[account_of_interest]['cash']['value']:
-	print("You don't have enough cash to invest that much.\n")
-	amount_to_contribute = int(input("Enter a valid amount to use:\n>>> "))
-
-etfs_to_purchase, money_remaining = wSimple.getSharesToPurchase(my_holdings[my_holding_keys[0]], etf_id_dict, amount_to_contribute, True)
-
-
-print("\n\nIf you want to use $" + str(amount_to_contribute) + ", you should purchase:\n")
-for key in etfs_to_purchase:
-	if (etf_id_dict[key]['to_buy'] != 0):
-		print(etf_id_dict[key]['symbol'] + ": " + str(etf_id_dict[key]['to_buy']) + " at $" + str(etf_id_dict[key]['price']) + " for " + str(round(etf_id_dict[key]['to_buy'] * etf_id_dict[key]['price'],2)))
-
-print("\nWhich will leave $" + str(round(money_remaining,2)) + " uninvested out of the $" + str(amount_to_contribute) + " total.")
-
-to_purchase = input(str("\n\nDo you want to buy the above securities (y / n) in your "+ my_holdings[account_of_interest]['type'] + "?\n>>> ")).upper()
-
-if (to_purchase == "Y"):
-	print("\n")
-	for key in etfs_to_purchase:
-		if (etf_id_dict[key]['to_buy'] != 0):
-			acc_id = account_of_interest
-			sec_id = key
-			quantity = etf_id_dict[key]['to_buy']
-			price = etf_id_dict[key]['price']
-			data = wSimple.buy(tradeToken, acc_id, sec_id, quantity, price)
-			print(data['symbol'] + ": " + str(data['quantity']) + " purchased from " + data['account_id'])
-
-else:
-	print("\nNothing purchased\n")
-
-
+	else:
+		print("\nInvalid input.\n")
