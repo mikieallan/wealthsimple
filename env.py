48a5a5979e7cbc39bdde10962f63e98ae33896a3
Mikie Allan <mikieallan@gmail.com>
2022-01-13 21:08:44 -0500

    Add env file to repo

---
+++ env.py
@@ -0,0 +1,17 @@
+### Constants
+# Base URLs
+BASE_URL = "https://api.production.wealthsimple.com"
+TRADE_BASE_URL = "https://trade-service.wealthsimple.com"
+
+# Misc
+CLIENT_ID = "4da53ac2b03225bed1550eba8e4611e086c7b905a3855e6ed12ea08c246758fa"
+TRADE_CLIENT_ID = "qTgfOYKjgj_qOFKyo-AgMauB_GzywmKXoPUUDS4rF04"
+SCOPES = "invest.read invest.write mfda.read mfda.write mercer.read mercer.write trade.read trade.write empower.read empower.write tax.read tax.write"
+
+# Endpoints
+LOGIN_ENDPOINT = "/v1/oauth/token"
+SWITCH_ENDPOINT = "/v1/oauth/switch"
+POSITIONS_ENDPOINT = "/account/positions"
+WATCHLIST_ENDPOINT = "/watchlist"
+ACCOUNT_LIST_ENDPOINT = "/account/list"
+ORDERS_ENDPOINT = "/orders"
